69e655c545e39ffd284979a98f5d8d80b7a4506f
laurenperez <laurenashleymarks@gmail.com>
2017-09-21 15:41:51 -0700

    a working cloud app

--- index.js
+++ index.js
@@ -1,12 +1,26 @@
 var express = require('express');
+var multer = require('multer');
+//allows us to make a multipart form
+var upload = multer({dest: './uploads/'});
+var cloudinary = require('cloudinary');
 var ejsLayouts = require('express-ejs-layouts');
+require('dotenv').config()
 var app = express();
 
 app.set('view engine', 'ejs');
 app.use(ejsLayouts);
 
+var images = [];
+
 app.get('/', function(req, res) {
-  res.render('index');
+  res.render('index', {images, cloudinary});
+});
+
+app.post('/', upload.single('myFile'), function(req, res){
+  cloudinary.uploader.upload(req.file.path, function(result){
+    images.push(result.public_id);
+    res.redirect('/');
+  });
 });
 
 app.listen(3000);
